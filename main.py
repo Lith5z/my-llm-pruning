7f7cf31f4885dd620545d77d7008ab5fa537f031
Lith5z <18917633442@163.com>
2026-09-07 11:48:10 +0800

    Init venv

---
+++ main.py
@@ -0,0 +1,9 @@
+import torch
+
+if torch.cuda.is_available():
+    print(f"PyTorch version: {torch.__version__}")
+    print(f"CUDA version: {torch.version.cuda}")
+    print(f"GPU count: {torch.cuda.device_count()}")
+    print(f"Current GPU: {torch.cuda.get_device_name(0)}")
+else:
+    print("No CUDA device detected.")
